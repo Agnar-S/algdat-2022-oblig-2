b0fa6a8407621eb6ab7d4aa479cb827d3747c2c5
PedramZi <91465878+PedramZi@users.noreply.github.com>
2022-10-03 21:30:19 +0200

    opp1 er kodet ferdig og kommentert

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -37,13 +37,57 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     private int endringer;         // antall endringer i listen
 
     public DobbeltLenketListe() {
-        throw new UnsupportedOperationException();
+        hode = hale = null;
+        antall = 0;
+        endringer = 0;
     }
 
     public DobbeltLenketListe(T[] a) {
-        if(a == null){ throw new NullPointerException("Tabell a er null!"); //feilmedling sendes om tabellen er null
+        if (a == null) {
+            throw new NullPointerException("Tabell a er null!"); //feilmedling sendes om tabellen er null
         }
+
+        a = nullfjerner(a);       //setter listen a gjennom nullfjerner for at det skal ikke være nuller blant tallene
+
+        Node<T> forste = new Node<>(a[0]);       //første noden opprettes og settes i a[0]
+        antall++;                                //antall økes
+        hode = forste;                           //første node blir satt som hode
+
+        for (int i = 1; i < a.length; i++) {     //startes på 1 siden allerede hode er tatt på første
+            Node<T> neste = new Node<>(a[i]);    //oppretter nye noder i sine verdier
+            antall++;
+            forste.neste = neste;               //nye noder blir satt etter forste >> peker frem til neste node
+            neste.forrige = forste;             //setter neste sin forrige peker  >> siden dobbelt lenket så pekes til den forrige o
+            forste = neste;                     //og siden det skal lages noder for alle verdier så settes neste som forste
+            // altså a[0] = forste så skjer forste = neste blir slik at vi går opp a[i]
+        }
+        //til slutt blir vi tom av verdier og går ut av for_løkka og da settes siste verdi lik forste
+        hale = forste;
     }
+
+        private T[] nullfjerner (T[] a){
+            int antall_nuller = 0;
+            for (T verdi : a) {
+                if (verdi == null) {                  // Teller hvor maneg nuller det finnes
+                    antall_nuller++;
+                }
+            }
+            if (antall_nuller == 0) {
+                return a;
+            }     //Om det ikke finnes nuller i lista så returneres selve lista
+
+            T[] b = (T[]) new Object[a.length - antall_nuller];     // antall nuller subtraheres fra liste sin lengde
+
+            int j = 0;
+
+            for (int i = 0; i < a.length; i++) { //en for løkke som lopper for å fjerne nuller eller å bare sette tall ved
+                if (a[i] != null) {               // siden av hverandre
+                    b[j] = a[i];
+                    j++;
+                }
+            }
+            return b;
+        }
 
     public Liste<T> subliste(int fra, int til) {
         throw new UnsupportedOperationException();
